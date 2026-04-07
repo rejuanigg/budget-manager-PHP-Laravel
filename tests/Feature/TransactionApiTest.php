9273ad2b67311531eda928407a40a1c800bba20d
rejuanigg <riosjuan32223@gmail.com>
2026-04-07 15:32:15 -0300

    test(basicTesting): create the test destroy transaction api in tests

--- tests/Feature/TransactionApiTest.php
+++ tests/Feature/TransactionApiTest.php
@@ -1,6 +1,7 @@
 <?php
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Transaction;
 
 test('Test Store Transaction API', function () {
     $user = User::factory()->create();
@@ -23,4 +24,27 @@ test('Test Store Transaction API', function () {
     );
 
     $response->assertStatus(201);
+});
+
+test('Test Destroy Transaction API', function(){
+    $user = User::factory()->create();
+
+    $category = Category::create([
+    'name' => 'Test',
+    'user_id' => $user->id
+    ]);
+
+    $transaction = Transaction::create([
+        'user_id' => $user->id,
+        'transaction_date' => '2023-02-02',
+        'type' => 'expense',
+        'detail' => 'Testing Api',
+        'amount' => '9999',
+        'category_id' => $category->id
+    ]);
+
+    $response = $this->actingAs($user)
+    ->deleteJson("api/transactions/{$transaction->id}");
+
+    $response->assertStatus(204);
 });
